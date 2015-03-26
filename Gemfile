79cb59d4478cc2dcef3958f2573eccced121435f
Shicong Liu <liushicong0511@gmail.com>
2015-03-26 15:19:11 -0500

    Bootstrap Install

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'devise'
 gem 'execjs'
 gem 'therubyracer'
 
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   root 'welcome#index'
+=======
+  devise_for :users
+  #resources :users
+  root 'users#index'
+>>>>>>> Stashed changes
 
 end
