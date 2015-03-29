70aa4801c2100411b1e14f2d64fcf93ccbd93629
schkott <saewebdev@gmail.com>
2015-03-29 18:01:27 -0500

    Fixed Bootstrap, added Highcharts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   root 'welcome#index'
-=======
-  devise_for :users
-  #resources :users
-  root 'users#index'
->>>>>>> Stashed changes
-
 end
